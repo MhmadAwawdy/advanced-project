62fb1012f679ca88b6237291f3a35be978ca8b7f
LanaJaara <s12011152@stu.najah.edu>
2024-12-15 17:06:16 +0200

    update-book

---
+++ src/main/java/org/openjfx/librarysystem/app/UpdateController.java
@@ -0,0 +1,14 @@
+package org.openjfx.librarysystem.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UpdateController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
